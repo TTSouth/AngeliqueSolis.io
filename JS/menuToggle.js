c7034c68753992db5c01c13e97b7502e3860536b
Tanya Thomas <89400178+TTSouth@users.noreply.github.com>
2024-08-07 10:07:03 -0400

    Add files via upload

---
+++ JS/menuToggle.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+
+let x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
